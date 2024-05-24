2183bab61dc7a9c0d0b0b0211619a49e34a519d5
briotza <cianomeliunas@gmail.com>
2024-05-24 11:12:05 -0300

    feat: navbar e icons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
+import logo from './assets/img/logo.png';
 
 function App() {
   return (
     <div className="container-fluid page-container">
       <div className="row">
       <nav className="navbar navbar-light nav-bar">
-          <span className="navbar-brand mb-0 h1">Sync360</span>
+          <img src={logo} alt='logo-comunishare' className='logo'/>
+          <div>
+            <button>Procurar</button>
+            <button>Sobre</button>
+            <button>Login</button>
+          </div>
           <button className="navbar-toggler d-md-none nav-btn" type="button">
             <span className="navbar-toggler-icon"></span>
           </button>
         </nav>
       </div>
-      <h1>Oii</h1>
+      <h1></h1>
     </div>
   );
 }
