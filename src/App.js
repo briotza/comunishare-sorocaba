966dbec2df32552585b6684ad1cd029d42ebe8fb
briotza <cianomeliunas@gmail.com>
2024-05-24 10:52:44 -0300

    feat: instalei bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="container-fluid page-container">
+      <div className="row">
+      <nav className="navbar navbar-light nav-bar">
+          <span className="navbar-brand mb-0 h1">Sync360</span>
+          <button className="navbar-toggler d-md-none nav-btn" type="button">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
       <h1>Oii</h1>
     </div>
   );
