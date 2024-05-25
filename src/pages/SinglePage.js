175797e79596e4c26573d98ac868caac6b9d7929
briotza <cianomeliunas@gmail.com>
2024-05-25 17:58:29 -0300

    feat: página volta para topo

--- src/pages/SinglePage.js
+++ src/pages/SinglePage.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
 import Foto from "../assets/img/no-pic.png";
 import Social from "../assets/img/social.png"
 import "../App.css";
 
 function SinglePage() {
 
+    const location = useLocation();
+
+    useEffect(() => {
+      // Scroll para o topo quando a rota muda
+      window.scrollTo(0, 0);
+    }, [location]);
 
     return (
         <div>
