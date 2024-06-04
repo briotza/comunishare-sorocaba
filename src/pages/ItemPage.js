0b10a0ea171ba45d4108a942aa947ea0a11c6622
briotza <cianomeliunas@gmail.com>
2024-06-04 15:53:43 -0300

    style: página do item atualizada

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom'; 
 import Foto from "../assets/img/no-pic.png";
+import Social from "../assets/img/social.png";
 import "../App.css";
 
 function ItemPage() {
@@ -13,7 +14,80 @@ function ItemPage() {
     }, [location]);
 
     return (
-        <p>Página do item</p>
+      <div>
+      <div className="col-md-12 d-flex flex-row justify-content-center">
+        <div className="col-md-4 intro">
+          <div className="left d-flex flex-column justify-content-center mb-4 mt-5">
+            <span className="d-block mb-2 h1">Nome do Item</span>
+            <p className="mt-3 h4">R$ 999,99</p>
+            <p className="mt-0">
+              Descrição: Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna.Descrição: Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna.Descrição: Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna.Descrição: Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor.
+            </p>
+          </div>
+        </div>
+        <div className="col-md-4 right d-flex flex-column justify-content-center align-items-center">
+          <img src={Foto} alt="Imagem exemplo" className="img-fluid mt-5" />
+        </div>
+      </div>
+
+      <div className="col-md-12 d-flex flex-column align-items-center mt-5 pt-4 pb-4 search">
+        <span className="d-block mb-0 h2 pt-4">Nome da Loja</span>
+        
+      </div>
+
+      <div className="col-md-12 d-flex flex-column align-items-center mt-3 pt-5 recommendations">
+        <div className="d-flex flex-column flex-sm-row mt-4">
+          <Link className="a" to="/item">
+            <button className="d-flex flex-column btn-item">
+              <img src={Foto} alt="Imagem exemplo" className="img-fluid mb-2" />
+              <span>produto/serviço</span>
+              <p>R$ 999,99</p>
+            </button>
+          </Link>
+          <Link className="a" to="/item">
+            <button className="d-flex flex-column btn-item">
+              <img src={Foto} alt="Imagem exemplo" className="img-fluid mb-2" />
+              <span>produto/serviço</span>
+              <p>R$ 999,99</p>
+            </button>
+          </Link>
+          <Link className="a" to="/item">
+            <button className="d-flex flex-column btn-item">
+              <img src={Foto} alt="Imagem exemplo" className="img-fluid mb-2" />
+              <span>produto/serviço</span>
+              <p>R$ 999,99</p>
+            </button>
+          </Link>
+        </div>
+        <div className="d-flex flex-column flex-sm-row mt-4">
+        <Link className="a" to="/item">
+            <button className="d-flex flex-column btn-item">
+              <img src={Foto} alt="Imagem exemplo" className="img-fluid mb-2" />
+              <span>produto/serviço</span>
+              <p>R$ 999,99</p>
+            </button>
+          </Link>
+          <Link className="a" to="/item">
+            <button className="d-flex flex-column btn-item">
+              <img src={Foto} alt="Imagem exemplo" className="img-fluid mb-2" />
+              <span>produto/serviço</span>
+              <p>R$ 999,99</p>
+            </button>
+          </Link>
+          <Link className="a" to="/item">
+            <button className="d-flex flex-column btn-item">
+              <img src={Foto} alt="Imagem exemplo" className="img-fluid mb-2" />
+              <span>produto/serviço</span>
+              <p>R$ 999,99</p>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
     );
 }
 
