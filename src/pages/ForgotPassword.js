9e04b0d171e03b3b4a03dd740b2a267cdac9d606
briotza <cianomeliunas@gmail.com>
2024-06-06 12:31:27 -0300

    style: página de esqueci a senha

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Link, useNavigate } from "react-router-dom";
-import Foto from "../assets/img/no-pic.png";
-import Social from "../assets/img/social.png";
 import "../App.css";
 
 function ForgotPassword() {
@@ -13,9 +11,30 @@ function ForgotPassword() {
   }, [location]);
 
   return (
-  
-<p>Oi</p>
-);
-};
+    <div className="d-flex flex-row justify-content-center page-login">
+      <div className="col-md-4 intro">
+        <div className="left d-flex flex-column justify-content-center ">
+          <span className="d-block mb-4 h2">Esqueci a senha</span>
+          <form >
+            <div className="form-group mb-1">
+              <label htmlFor="email">Um código foi enviado para seu e-mail de cadastro</label>
+              <input
+                type="codigo"
+                className="form-control mt-2 mb-4"
+                id="codigo"
+                placeholder="Digite o código"
+                required
+              />
+              <span>(Código: 1234)</span>
+            </div>
+            <button type="submit" className="btn btn-primary mt-3 mb-5">
+              Ok
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default ForgotPassword;
